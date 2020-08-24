36ecca6253e40ae2f6d2c83fd47b404c59e50731
Julian Berman <Julian@GrayVines.com>
2020-08-24 18:43:01 -0400

    Colored pieces maybe.

--- src/chess/piece.lean
+++ src/chess/piece.lean
@@ -7,8 +7,8 @@ Chess piece implementation.
 namespace chess
 
 inductive color
-| white : color
-| black : color
+| white
+| black
 
 inductive pieces
 | bishop
@@ -18,15 +18,36 @@ inductive pieces
 | queen
 | rook
 
-namespace pieces
+class colored_pieces :=
+(piece : pieces)
+(color : color)
 
-  notation ` ♔ ` := king
-  notation ` ♕ ` := queen
-  notation ` ♖ ` := rook
-  notation ` ♗ ` := bishop
-  notation ` ♘ ` := knight
-  notation ` ♙ ` := pawn
+instance white_bishop : colored_pieces := ⟨pieces.bishop, color.white⟩
+instance white_king : colored_pieces := ⟨pieces.king, color.white⟩
+instance white_knight : colored_pieces := ⟨pieces.knight, color.white⟩
+instance white_pawn : colored_pieces := ⟨pieces.pawn, color.white⟩
+instance white_queen : colored_pieces := ⟨pieces.queen, color.white⟩
+instance white_rook : colored_pieces := ⟨pieces.rook, color.white⟩
 
-end pieces
+instance black_bishop : colored_pieces := ⟨pieces.bishop, color.black⟩
+instance black_king : colored_pieces := ⟨pieces.king, color.black⟩
+instance black_knight : colored_pieces := ⟨pieces.knight, color.black⟩
+instance black_pawn : colored_pieces := ⟨pieces.pawn, color.black⟩
+instance black_queen : colored_pieces := ⟨pieces.queen, color.black⟩
+instance black_rook : colored_pieces := ⟨pieces.rook, color.black⟩
+
+notation ` ♔ ` := chess.white_king
+notation ` ♕ ` := chess.white_queen
+notation ` ♖ ` := chess.white_rook
+notation ` ♗ ` := chess.white_bishop
+notation ` ♘ ` := chess.white_knight
+notation ` ♙ ` := chess.white_pawn
+
+notation ` ♚ ` := chess.black_king
+notation ` ♛ ` := chess.black_queen
+notation ` ♜ ` := chess.black_rook
+notation ` ♝ ` := chess.black_bishop
+notation ` ♞ ` := chess.black_knight
+notation ` ♟︎ ` := chess.black_pawn
 
 end chess
