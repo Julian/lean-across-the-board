eace3d7b63264a4cc17aece0dea14a0a040fca0a
Julian Berman <Julian@GrayVines.com>
2020-08-28 18:00:46 -0400

    Add chess.board.
    With a heap of help via the zulip chat from Yakov Pechersky.

--- src/chess/board.lean
+++ src/chess/board.lean
@@ -1,11 +1,33 @@
-/-
+import data.matrix.notation
+import chess.piece
+
+/-!
 
 Straightforward implementation of a chess board.
 
 -/
 
-import chess.piece
-
 namespace chess
+
+variables (m n : Type) [fintype n] [decidable_eq n] [fintype m] [decidable_eq m]
+variables (ι : Type) [fintype ι] [decidable_eq ι]
+
+variables (K : Type*)
+
+/--
+A board is axiomatized as a set of indexable (ergo distinguishable) pieces
+which are placed on distinct squares.
+-/
+structure board :=
+(pieces : ι → K)
+(contents : matrix m n (option ι))
+(contains_pieces : ∀ ix : ι, ∃ x : m, ∃ y : n, contents x y = ix)
+(no_superimposed_pieces :
+  ∀ (x x' : m),
+  ∀ (y y' : n),
+    x ≠ x' → y ≠ y' →
+    (contents x y).is_some →
+    (contents x' y').is_some →
+      contents x y ≠ contents x' y')
 
 end chess
