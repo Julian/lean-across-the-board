ed7685873ffec3273bb80a475c4dac992bd56609
Yakov Pechersky <yakov@pechersky.us>
2020-09-15 03:54:41 +0000

    Clean up in tests
    Switch `end_square` in `downright` to (2, 1) since `2 : fin 2 = 0 : fin 2`.

--- test/chess.lean
+++ test/chess.lean
@@ -6,8 +6,8 @@ def b₁ : chess.board _ _ _ _ :=
     pieces := ![♞],
     contents := PF ![![(0 : fin 1), __], ![__, __], ![__, __]] }
 
-example : chess.board.width b₁ = 2 := by refl
-example : chess.board.height b₁ = 3 := by refl
+example : chess.board.width b₁ = 2 := rfl
+example : chess.board.height b₁ = 3 := rfl
 
 example (p p' q q' r r' : chess.colored_pieces) : chess.board _ _ _ _ :=
   { chess.board .
@@ -21,4 +21,4 @@ example (p p' q q' r r' : chess.colored_pieces) : chess.board _ _ _ _ :=
     no_superimposed_pieces := dec_trivial, }
 
 
-def downright : chess.move b₁ := {start_square := (0, 0), end_square := (1, 2)}
+def downright : chess.move b₁ := {start_square := (0, 0), end_square := (2, 1)}
