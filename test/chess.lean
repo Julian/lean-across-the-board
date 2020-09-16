808b27be3e3eda97cc1202d9e3732349fe0f7347
Yakov Pechersky <yakov@pechersky.us>
2020-09-16 01:49:34 +0000

    Provide local numerical notation for `option (fin n)`
    This allows cleaner writing of playfield states. Instead of relying on
    coercion via explicit type annotation, rely instead on local instances
    that make `bit0` and `bit1` work.

--- test/chess.lean
+++ test/chess.lean
@@ -1,10 +1,29 @@
 import chess.move
 
+/-!
+We define local instances for `option (fin n)` for a notational shortcut.
+This allows us to write 0, 1, ... for valid `option (fin n)`.
+Then, it is easy to use `fin n` as an index type in our boards.
+-/
+
+/-- Provide a zero for `option (fin (n + 1))`. -/
+def option.fin.zero {n : ℕ} : has_zero (option (fin (n + 1))) := ⟨some 0⟩
+local attribute [instance] option.fin.zero
+
+/-- Provide a onm for `option (fin (n + 2))`. -/
+def option.fin.one {n : ℕ} : has_one (option (fin (n + 2))) := ⟨some 1⟩
+local attribute [instance] option.fin.one
+
+/-- Provide addition for `option (fin n)`, which is only on the `some` values,
+and shortcircuits on `none`. Given that `0` and `1` are locally defined for
+`option (fin (n + 2))`, this gives access to other numerals via `bit0` and `bit1`. -/
+def option.fin.add {n : ℕ} : has_add (option (fin n)) := ⟨λ a b, (+) <$> a <*> b⟩
+local attribute [instance] option.fin.add
 
 def b₁ : chess.board _ _ _ _ :=
   { chess.board .
     pieces := ![♞],
-    contents := PF ![![(0 : fin 1), __], ![__, __], ![__, __]] }
+    contents := PF ![![0 , __], ![__, __], ![__, __]] }
 
 example : chess.board.width b₁ = 2 := rfl
 example : chess.board.height b₁ = 3 := rfl
@@ -13,9 +32,9 @@ example (p p' q q' r r' : chess.colored_pieces) : chess.board _ _ _ _ :=
   { chess.board .
     pieces := ![p, p', q, q', r, r'],
     contents := PF ![
-      ![__, __, (0 : fin 6), (5 : fin 6), __, __],
-      ![(1 : fin 6), __, (3 : fin 6), __, __, __],
-      ![__, __, __, __, (2 : fin 6), (4 : fin 6)]
+      ![__, __, 0 , 5 , __, __],
+      ![1 , __, 3 , __, __, __],
+      ![__, __, __, __, 2 , 4 ]
     ],
     contains_pieces := dec_trivial,
     no_superimposed_pieces := dec_trivial, }
