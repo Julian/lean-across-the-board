8e6db24fa6ae8b4751ed736e40e9e334a0b4feaf
Yakov Pechersky <yakov@pechersky.us>
2020-09-22 22:30:13 +0000

    Try out the repr in guarini

--- src/guarini.lean
+++ src/guarini.lean
@@ -1,4 +1,4 @@
-import chess.board
+import chess.move
 
 /-!
 
@@ -58,7 +58,7 @@ def ending_position : chess.board _ _ _ _ := {
 
 -- Direct solution
 
-def guarini_seq := [
+def guarini_seq : list ((fin 3 × fin 3) × (fin 3 × fin 3)) := [
   ((2, 0), (0, 1)),
   ((2, 2), (1, 0)),
   ((0, 1), (2, 2)),
@@ -76,6 +76,22 @@ def guarini_seq := [
   ((2, 1), (0, 0)),
   ((1, 2), (0, 2))
 ]
+
+def inhabited_fin3 : inhabited (fin 3) := ⟨0⟩
+local attribute [instance] inhabited_fin3
+
+def first_move : chess.move starting_position :=
+let pair := guarini_seq.head in ⟨pair.fst, pair.snd, dec_trivial, dec_trivial, dec_trivial⟩
+
+def fin_repr {α : Type*} [has_repr α] : Π {n}, (fin n → α) → string
+| 0       _ := ""
+| (n + 1) v := repr (matrix.vec_head v) ++ ", " ++ fin_repr (matrix.vec_tail v)
+
+instance fin_repr_instance {α : Type*} {n : ℕ} [has_repr α] : has_repr (fin n → α) :=
+⟨fin_repr⟩
+
+#eval starting_position.pieces
+
 
 /-  Pseudo-proof of a direct solution
 lemma starting_position.exists_move_seq ending_position := begin
