bfb23bd56e7352c8692b95fba27f524d0f75e9c9
Yakov Pechersky <yakov@pechersky.us>
2020-09-22 22:30:01 +0000

    Provide pieces with a string representation

--- src/chess/piece.lean
+++ src/chess/piece.lean
@@ -21,23 +21,23 @@ inductive pieces
 | rook
 
 @[derive decidable_eq]
-class colored_pieces :=
+structure colored_pieces :=
 (piece : pieces)
 (color : color)
 
-instance white_bishop : colored_pieces := ⟨pieces.bishop, color.white⟩
-instance white_king : colored_pieces := ⟨pieces.king, color.white⟩
-instance white_knight : colored_pieces := ⟨pieces.knight, color.white⟩
-instance white_pawn : colored_pieces := ⟨pieces.pawn, color.white⟩
-instance white_queen : colored_pieces := ⟨pieces.queen, color.white⟩
-instance white_rook : colored_pieces := ⟨pieces.rook, color.white⟩
+def white_bishop : colored_pieces := ⟨pieces.bishop, color.white⟩
+def white_king : colored_pieces := ⟨pieces.king, color.white⟩
+def white_knight : colored_pieces := ⟨pieces.knight, color.white⟩
+def white_pawn : colored_pieces := ⟨pieces.pawn, color.white⟩
+def white_queen : colored_pieces := ⟨pieces.queen, color.white⟩
+def white_rook : colored_pieces := ⟨pieces.rook, color.white⟩
 
-instance black_bishop : colored_pieces := ⟨pieces.bishop, color.black⟩
-instance black_king : colored_pieces := ⟨pieces.king, color.black⟩
-instance black_knight : colored_pieces := ⟨pieces.knight, color.black⟩
-instance black_pawn : colored_pieces := ⟨pieces.pawn, color.black⟩
-instance black_queen : colored_pieces := ⟨pieces.queen, color.black⟩
-instance black_rook : colored_pieces := ⟨pieces.rook, color.black⟩
+def black_bishop : colored_pieces := ⟨pieces.bishop, color.black⟩
+def black_king : colored_pieces := ⟨pieces.king, color.black⟩
+def black_knight : colored_pieces := ⟨pieces.knight, color.black⟩
+def black_pawn : colored_pieces := ⟨pieces.pawn, color.black⟩
+def black_queen : colored_pieces := ⟨pieces.queen, color.black⟩
+def black_rook : colored_pieces := ⟨pieces.rook, color.black⟩
 
 notation ` ♔ ` := chess.white_king
 notation ` ♕ ` := chess.white_queen
@@ -54,5 +54,21 @@ notation ` ♞ ` := chess.black_knight
 notation ` ♟︎ ` := chess.black_pawn
 
 notation ` __ ` := none
+
+def piece_repr : colored_pieces → string
+| ⟨pieces.king, color.white⟩ := "♔"
+| ⟨pieces.queen, color.white⟩ := "♕"
+| ⟨pieces.rook, color.white⟩ := "♖"
+| ⟨pieces.bishop, color.white⟩ := "♗"
+| ⟨pieces.knight, color.white⟩ := "♘"
+| ⟨pieces.pawn, color.white⟩ := "♙"
+| ⟨pieces.king, color.black⟩ := "♚"
+| ⟨pieces.queen, color.black⟩ := "♛"
+| ⟨pieces.rook, color.black⟩ := "♜"
+| ⟨pieces.bishop, color.black⟩ := "♝"
+| ⟨pieces.knight, color.black⟩ := "♞"
+| ⟨pieces.pawn, color.black⟩ := "♟︎"
+
+instance : has_repr colored_pieces := ⟨piece_repr⟩
 
 end chess
