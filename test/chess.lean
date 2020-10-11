d0e523a987ef85acbedc228396f02081bf7998ff
Julian Berman <Julian@GrayVines.com>
2020-10-11 09:29:39 -0400

    Tidy up the tests into sections.

--- test/chess.lean
+++ test/chess.lean
@@ -10,7 +10,7 @@ Then, it is easy to use `fin n` as an index type in our boards.
 def option.fin.zero {n : ℕ} : has_zero (option (fin (n + 1))) := ⟨some 0⟩
 local attribute [instance] option.fin.zero
 
-/-- Provide a onm for `option (fin (n + 2))`. -/
+/-- Provide a one for `option (fin (n + 2))`. -/
 def option.fin.one {n : ℕ} : has_one (option (fin (n + 2))) := ⟨some 1⟩
 local attribute [instance] option.fin.one
 
@@ -20,6 +20,14 @@ and shortcircuits on `none`. Given that `0` and `1` are locally defined for
 def option.fin.add {n : ℕ} : has_add (option (fin n)) := ⟨λ a b, (+) <$> a <*> b⟩
 local attribute [instance] option.fin.add
 
+section piece
+
+example : ♞ ≠ ♚ := dec_trivial
+
+end piece
+
+section board
+
 def b₁ : chess.board _ _ _ _ :=
   { chess.board .
     pieces := ![♞],
@@ -28,15 +36,8 @@ def b₁ : chess.board _ _ _ _ :=
 example : chess.board.width b₁ = 2 := rfl
 example : chess.board.height b₁ = 3 := rfl
 
-def downright : chess.move b₁ := {start_square := (0, 0), end_square := (2, 1)}
-
 example : 0 ∈ b₁.contents := dec_trivial
 example : 0 ∈ b₁ := dec_trivial
-
-example : ♞ ≠ ♚ := dec_trivial
-
-example : downright.piece = ♞ := rfl
-example : downright.piece ≠ ♚ := dec_trivial
 
 variables {p p' q q' r r' : chess.colored_pieces}
 
@@ -48,3 +49,14 @@ def b₂ : chess.board _ _ _ _ :=
       ![1 , __, 3 , __, __, __],
       ![__, __, __, __, 2 , 4 ]
     ] }
+
+end board
+
+section move
+
+def downright : chess.move b₁ := {start_square := (0, 0), end_square := (2, 1)}
+
+example : downright.piece = ♞ := rfl
+example : downright.piece ≠ ♚ := dec_trivial
+
+end move
