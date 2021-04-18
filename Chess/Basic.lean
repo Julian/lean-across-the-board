fe6a3ac40c311ec398853d45905d2f885051ed40
Julian Berman <Julian@GrayVines.com>
2021-04-18 14:56:40 -0400

    Super basic boardish thing.

--- Chess/Basic.lean
+++ Chess/Basic.lean
@@ -1,1 +1,16 @@
-structure Board
+variable (P : Type) [BEq P]
+
+structure Board where
+  contents : Array (Array P)
+  contiguous (rank : Nat) : contents[rank].size ≠ 0
+  nontrivial : contents ≠ #[]
+  rectangular : ∃ n, ∀ rank, contents[rank].size = n
+
+namespace Board
+
+variable (B : Board P)
+
+def width := B.contents[0].size
+def height := B.contents.size
+
+end Board
